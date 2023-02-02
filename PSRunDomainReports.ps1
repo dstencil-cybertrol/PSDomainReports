90890233dc8e9059454268f05586c415193cfbc4
dstencil-cybertrol <104449382+dstencil-cybertrol@users.noreply.github.com>
2023-02-02 21:18:07 +0000

    Update PSRunDomainReports.ps1

--- PSRunDomainReports.ps1
+++ PSRunDomainReports.ps1
@@ -1,12 +1,4 @@
-$scriptsList =
-@(
-'C:\temp\Get-NestedGroupsSearch.ps1'
-'C:\temp\Get-ADMembersSearch.ps1'
-'C:\temp\Get-DomainHotfixInstalledSearch.ps1'
-'C:\temp\Get-SoftwareInstalledSearch.ps1'
-)
-
-foreach($script in $scriptsList)
-{
-Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList "-command '& $script'" -Wait
-} 
+&"$PSScriptroot\Get-NestedGroupsSearch.ps1"
+&"$PSScriptroot\Get-ADMembersSearch.ps1"
+&"$PSScriptroot\Get-SoftwareInstalledSearch.ps1"
+&"$PSScriptroot\Get-DomainHotfixInstalledSearch.ps1"
